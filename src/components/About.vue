e5c86fb8f2e1ec4ae1ac4e685066f7bb4e27c0da
axreer <saidaxrorpc7@gmail.com>
2024-05-16 20:50:59 +0500

    changed

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,21 +10,19 @@
                 </div>
 
                 <div class="about__right">
-                    <h2 class="about__title">About <span>Me</span></h2>
+                    <h2 class="about__title">Men <span>haqimda</span></h2>
                     <p class="about__text">
-                        Hi I'm a UX designer, prototyper, and a part-time 3D artist. I'm currently a Working at Fraxos, working remotely from Boston.
-                        If you're a creative in Boston, say hi!
+                        Salom, mening ismim <span>Saidaxror</span>, men 2005-yil Andijon viloyatida tavallud topganman. Bugungi kunda Front-end
+                        dasturchiman.
                         <br />
                         <br />
-                        Most recently, I was a Senior UX Designer at Delphia. Before that, I worked at startups in Seoul such as Dable, Pinch,
-                        Creatrip, and Dotface. Before that, I was a design intern at Adobe in San Francisco.
+                        Asosan bugungi kunda men UPWORK freelancing platformasida freelancer sifatida ish olib boraman va shu kungacha juda ko'plab
+                        muvoffaqiyatli proektlarni o'z egalariga topshirganman!
                         <br />
                         <br />
-                        I love words that start with P: people, process, problem solving, paper sketches, prototyping, programming, polished design,
-                        pixel-perfect products, design system, UI animation, and Simulated Reality
+                        Men responsive veb-saytlar, mobil ilovalar, veb ilovalar, veb komponentlar, va animatsiyalar yarataman.
                     </p>
                 </div>
-
             </div>
         </div>
     </section>
@@ -35,31 +33,29 @@
                 <div class="info__card">
                     <div class="info__left">
                         <img src="@/assets/img/books.svg" alt="" />
-                        <p>Education</p>
+                        <p>Ta`lim</p>
                     </div>
 
                     <div class="info__right">
                         <div class="info__detail">
-                            <span class="info__date date__media">2023 - current</span>
+                            <span class="info__date date__media">2023 - <i class="fal fa-hourglass-half"></i></span>
                             <div class="info__texts">
-                                <p class="info__name">Institute of economics and service, Samarkand</p>
-                                <p class="info__sub">Bachelor of Science, Marketing</p>
+                                <p class="info__name">Samarqand Iqtisodiyot va Servis Instituti</p>
+                                <p class="info__sub">Fanlar bo'yicha bakalavr, Marketing</p>
                                 <p class="info__desc">
-                                    Lorem Ipsum is simply dummy text of the printing and. Lorem Ipsum has been the industry's standard dummy text
-                                    unknown printer took a galley centuries,
+                                    Men O'zbekistonning eng yaxshi universitetlaridan biri bo'lgan Samarqand Iqtisodiyot va Servis Institutida 1-kurs talabasiman. Bu Instutda men Marketing yo'nalishi bo'yicha o'qiyman
                                 </p>
-                                <span class="info__date date__pc">2023 - current</span>
+                                <span class="info__date date__pc">2023 - <i class="fal fa-hourglass-half"></i></span>
                             </div>
                         </div>
 
                         <div class="info__detail">
-                            <span class="info__date date__media">2011 to 2012</span>
+                            <span class="info__date date__media">2021 - 2023</span>
                             <div class="info__texts">
-                                <p class="info__name">University of California, Berkeley</p>
-                                <p class="info__sub">Graduate of Visual Communication Design.</p>
+                                <p class="info__name">Bo'ston Ixtisoslashtirilgan Kollej</p>
+                                <p class="info__sub">Kompyuter fanlari va kompyuter muhandisligi</p>
                                 <p class="info__desc">
-                                    Lorem Ipsum is simply dummy text of the printing and. Lorem Ipsum has been the industry's standard dummy text
-                                    unknown printer took a galley centuries,
+                                    Andijon viloyatining Boston tumanidagi Chuqurlashtirilgan kollejda Kompyuter fanlari va Kompyuter muhandisligi bo'yicha barcha bilimlarni egallaganman.
                                 </p>
                                 <span class="info__date date__pc">2023 - current</span>
                             </div>
@@ -70,18 +66,17 @@
                 <div class="info__card">
                     <div class="info__left">
                         <img src="@/assets/img/bag.svg" alt="" />
-                        <p>Work Experience</p>
+                        <p>Ish Tajribasi</p>
                     </div>
 
                     <div class="info__right">
                         <div class="info__detail">
-                            <span class="info__date date__media">2022 current </span>
+                            <span class="info__date date__media">2023 - <i class="fal fa-hourglass-half"></i></span>
                             <div class="info__texts">
-                                <p class="info__name">Fraxos</p>
-                                <p class="info__sub">Seniour Design</p>
+                                <p class="info__name">UPWORK</p>
+                                <p class="info__sub">Front-End dasturchi</p>
                                 <p class="info__desc">
-                                    Lorem Ipsum is simply dummy text of the printing and. Lorem Ipsum has been the industry's standard dummy text
-                                    unknown printer took a galley centuries,
+                                    Bugungi kunda UPWORK platformasida freelancer sifatida faoliyat yuritaman. Shu kungacha juda ko'plab loyihalar  ustida ishlaganman va ishlayapman.
                                 </p>
                                 <span class="info__date date__pc">2022 current </span>
                             </div>
@@ -90,13 +85,12 @@
                         <div class="info__detail">
                             <span class="info__date date__media">2017 to 2019</span>
                             <div class="info__texts">
-                                <p class="info__name">Graphic Designer</p>
-                                <p class="info__sub">Branding & Graphic Design</p>
+                                <p class="info__name">ASR INNOVATION CENTER</p>
+                                <p class="info__sub">Grafik Dizayner</p>
                                 <p class="info__desc">
-                                    Lorem Ipsum is simply dummy text of the printing and. Lorem Ipsum has been the industry's standard dummy text
-                                    unknown printer took a galley centuries,
+                                    Asr Innavatsion O'quv markazida Grafik dizayner bo'lib ishlaganman. Juda ko'plab SMD postlarini yasaganman.
                                 </p>
-                                 <span class="info__date date__pc">2017 to 2019</span>
+                                <span class="info__date date__pc">2017 to 2019</span>
                             </div>
                         </div>
 
